989587cfc2bbdaaff1e0ef9636606d991f584317
João Speck <joao.speck87@gmail.com>
2024-03-19 15:57:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Convers-o-de-Horas-Web
+ Projeto_Horas_automação
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/calcular_diferencas_tempo', methods=['POST'])
+def calcular_diferencas_tempo():
+    data_inicio = request.json['dataInicio']
+    data_fim = request.json['dataFim']
+
+    diferenca_total = 0
+    for inicio, fim in zip(data_inicio, data_fim):
+        try:
+            data_inicio_dt = datetime.strptime(inicio, "%d/%m/%Y %H:%M")
+            data_fim_dt = datetime.strptime(fim, "%d/%m/%Y %H:%M")
+            diferenca = data_fim_dt - data_inicio_dt
+            minutos_totais = diferenca.total_seconds() / 60
+            diferenca_total += minutos_totais
+        except ValueError:
+            return jsonify({"erro": "Por favor, insira datas e horas válidas no formato especificado."}), 400
+
+    return jsonify(diferenca_total)
+
+if __name__ == '__main__':
+    app.run(debug=True)
